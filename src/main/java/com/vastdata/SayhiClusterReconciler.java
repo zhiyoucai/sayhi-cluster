e0a7b9aa33609922046350a75da6ad76576baa1f
zhiyoucai <zq.works@hotmail.com>
2022-04-20 14:37:49 +0800

    change

--- src/main/java/com/vastdata/SayhiClusterReconciler.java
+++ src/main/java/com/vastdata/SayhiClusterReconciler.java
@@ -12,14 +12,13 @@ import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SayhiClusterReconciler implements Reconciler<SayhiCluster> {
   private static final Logger LOGGER = LoggerFactory.getLogger(SayhiClusterReconciler.class);
   private final KubernetesClient client;
+
+  private String image;
 
   public SayhiClusterReconciler(KubernetesClient client) {
     this.client = client;
@@ -30,29 +29,30 @@ public class SayhiClusterReconciler implements Reconciler<SayhiCluster> {
   @Override
   public UpdateControl<SayhiCluster> reconcile(SayhiCluster resource, Context context) {
     // TODO: fill in logic
-    Deployment deployment = new Deployment();
-    deployment.setApiVersion("apps/v1");
-    deployment.setKind(ResourceTypeEnum.DEPLOYMENT.getType());
-    deployment.setSpec(buildDeploymentSpec());
-    ObjectMeta meta = new ObjectMeta();
-    meta.setName("sayhi");
-    deployment.setMetadata(meta);
+    // 先读取出CR定义的元数据，现在只有两个分别是副本数和Docker镜像
+    var size = resource.getSpec().getSize();
+    image = resource.getSpec().getImage();
 
-    Service service = new Service();
-    service.setApiVersion("v1");
-    service.setKind(ResourceTypeEnum.SERVICE.getType());
-    ObjectMeta serviceMeta = new ObjectMeta();
-    serviceMeta.setName("hello-sayhi");
-    service.setMetadata(serviceMeta);
-    service.setSpec(buildServiceSpec());
+    // 新建一个Deployment和一个Service
+    Deployment deployment = buildDeployment(size);
+    Service service = buildService();
 
+    // 拿到刚才新建的Deployment和Service的资源信息
     final var deploymentResource = client.resources(Deployment.class).withName("sayhi");
     final var serviceResource = client.resources(Service.class).withName("hello-sayhi");
+
+    // 判断Deployment和Service是否存在
     final var deploymentExisting = deploymentResource.get();
     final var serviceExisting = serviceResource.get();
+
+    // 得到Deployment的副本数量
+    var replicaSize = deploymentResource.get().getStatus().getReplicas();
+    LOGGER.info("Deployment {} has {} replicas", deploymentResource.get().getMetadata().getName(), replicaSize);
     try {
-      if (deploymentExisting == null) {
-        deploymentResource.create(deployment);
+      // 如果Deployment不存在就创建
+      // 如果副本数和CR定义的不一致，也修改
+      if (deploymentExisting == null || !checkReplicas(replicaSize, size)) {
+        deploymentResource.createOrReplace(deployment);
       }
       if (serviceExisting == null) {
         serviceResource.create(service);
@@ -61,6 +61,38 @@ public class SayhiClusterReconciler implements Reconciler<SayhiCluster> {
       LOGGER.error("", e);
     }
     return UpdateControl.noUpdate();
+  }
+
+  /**
+   * 检查Deployment的实际副本数和期望副本数是否相等
+   * @param size Deployment的实际副本数
+   * @param expectSize 期望的副本数
+   * @return 如果不相等则返回false
+   */
+  private boolean checkReplicas(Integer size, Integer expectSize) {
+    return Objects.equals(size, expectSize);
+  }
+
+  private Service buildService() {
+    Service service = new Service();
+    service.setApiVersion("v1");
+    service.setKind(ResourceTypeEnum.SERVICE.getType());
+    ObjectMeta serviceMeta = new ObjectMeta();
+    serviceMeta.setName("hello-sayhi");
+    service.setMetadata(serviceMeta);
+    service.setSpec(buildServiceSpec());
+    return service;
+  }
+
+  private Deployment buildDeployment(Integer size) {
+    Deployment deployment = new Deployment();
+    deployment.setApiVersion("apps/v1");
+    deployment.setKind(ResourceTypeEnum.DEPLOYMENT.getType());
+    deployment.setSpec(buildDeploymentSpec(size));
+    ObjectMeta meta = new ObjectMeta();
+    meta.setName("sayhi");
+    deployment.setMetadata(meta);
+    return deployment;
   }
 
   private ServiceSpec buildServiceSpec() {
@@ -84,8 +116,9 @@ public class SayhiClusterReconciler implements Reconciler<SayhiCluster> {
     return serviceSpec;
   }
 
-  private DeploymentSpec buildDeploymentSpec() {
+  private DeploymentSpec buildDeploymentSpec(Integer size) {
     DeploymentSpec deploymentSpec = new DeploymentSpec();
+    deploymentSpec.setReplicas(size);
     deploymentSpec.setSelector(buildLabelSelector());
     deploymentSpec.setTemplate(buildPodTemplateSpec());
     return deploymentSpec;
@@ -107,7 +140,7 @@ public class SayhiClusterReconciler implements Reconciler<SayhiCluster> {
     containerPorts.add(containerPort);
 
     Container container = new Container();
-    container.setImage("youcai/sayhi:1.0.0");
+    container.setImage(image);
     container.setName("sayhi");
     container.setImagePullPolicy(ImagePullPolicyEnum.IF_NOT_PRESENT.getPolicy());
     container.setPorts(containerPorts);
